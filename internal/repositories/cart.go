8c8f37b91617d9978621c7e0bd8268bd30fa55c0
Nutt Chairatana <nuttchai@outlook.com>
2022-11-27 23:17:49 +0700

    Remove Console

--- internal/repositories/cart.go
+++ internal/repositories/cart.go
@@ -2,14 +2,13 @@ package repositories
 
 import (
 	"github.com/nuttchai/go-rest/internal/models"
-	"github.com/nuttchai/go-rest/internal/shared/console"
 	"github.com/nuttchai/go-rest/internal/utils/context"
 )
 
 func (m *DBModel) GetItems(userId string) ([]*models.CartItem, error) {
 	ctx, cancel := context.WithTimeout(3)
 	defer cancel()
-	console.App.Log("GetItems: " + userId)
+
 	query := "select * from cart where owner_id = $1"
 	rows, err := m.DB.QueryContext(ctx, query, userId)
 	if err != nil {
