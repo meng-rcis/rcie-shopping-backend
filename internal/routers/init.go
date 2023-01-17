ba4c9d7891f4fe82d612d3e151a3b8f5b09393fc
Nutt Chairatana <nuttchai@outlook.com>
2023-01-18 02:30:40 +0700

    Remove Sample Code

--- internal/routers/init.go
+++ internal/routers/init.go
@@ -3,7 +3,6 @@ package routers
 import "github.com/labstack/echo"
 
 func InitRouters(e *echo.Echo) *echo.Echo {
-	initSampleRouterPath(e)
 	initAdminRouterPath(e)
 	initCartRouterPath(e)
 	initProductRouterPath(e)
