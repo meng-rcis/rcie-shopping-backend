89b8609f743d7ad007966bedf24311c7e5fb09b6
Nutt Chairatana <nuttchai@outlook.com>
2023-01-19 12:56:04 +0700

    Update Path

--- internal/routers/admin.go
+++ internal/routers/admin.go
@@ -15,7 +15,7 @@ func initAdminRouterPath(e *echo.Echo) *echo.Echo {
 	// NO.13 Search + Loop to recalculate the product internally with keyword = "" and limit = "100000" with random offset (prevent cache) ~ 5 calls/min + randomly choose and save 10~20 products into DB
 	// NO.14 Search + Loop to recalculate the product internally with keyword = "" and limit = "1000" with random offset (prevent cache) ~ 500 calls/min + randomly choose and save 10~20 products into DB
 
-	e.GET(api.CreatePath("admin/product/bulk"), handlers.AdminHandler.BulkUpdateProduct) // Like admin/product/bulk but no need to update DB
+	// e.GET(api.CreatePath("admin/product/bulk"), handlers.AdminHandler.BulkGetModifiedProduct) // Like admin/product/bulk but no need to update DB
 	// NO.15 Search + Loop to recalculate the product internally with keyword = "" and limit = "10000" with random offset (prevent cache) ~ 5 calls/min + choose and save all products into DB
 	// NO.16 Search + Loop to recalculate the product internally with keyword = "" and limit = "1000" with random offset (prevent cache) ~ 500 calls/min + choose and save all products into DB
 
