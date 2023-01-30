befc93a60ddcdb1d53d5434cb606d3fa2d1f7a14
Nutt Chairatana <nuttchai@outlook.com>
2023-01-31 01:08:06 +0700

    Update API Usage Test

--- internal/routers/cart.go
+++ internal/routers/cart.go
@@ -8,8 +8,8 @@ import (
 )
 
 func initCartRouterPath(e *echo.Echo) *echo.Echo {
-	e.GET(api.CreatePath("cart"), handlers.CartHandler.GetAllCartItems) // NO.1
-	e.POST(api.CreatePath("cart"), handlers.CartHandler.AddCartItem)    // NO.1
+	e.GET(api.CreatePath("cart"), handlers.CartHandler.GetAllCartItems)
+	e.POST(api.CreatePath("cart"), handlers.CartHandler.AddCartItem) // NO.1
 	e.PUT(api.CreatePath("cart"), handlers.CartHandler.UpdateCartItem)
 	e.DELETE(api.CreatePath("cart/:id"), handlers.CartHandler.RemoveCartItem)
 
